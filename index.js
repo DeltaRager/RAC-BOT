1e3cb0c24caefa4732f50f7f2a46acbc58a53776
DeltaRager <abhinavtj@gmail.com>
2022-05-07 01:33:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const fs = require('fs')
 const { Client, Intents } = require('discord.js');
 
+const path = require('path');
+
+const dirPath = path.resolve(__dirname, './src/events');
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync(dirPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
-	const event = require(`./src/events/${file}`);
+	const event = require(`${dirPath}/${file}`);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
 	} else {
